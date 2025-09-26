029e352e70d4fe1d188d9bba9ce2da8bced2ca49
itsdarrendsilva@gmail.com <itsdarrendsilva@gmail.com>
2025-09-26 11:56:02 +0530

    Added HelloInternship C# program

---
+++ HelloInternship/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloInternship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Internship!");
+            Console.WriteLine("Today is the begining");
+        }
+    }
+}
